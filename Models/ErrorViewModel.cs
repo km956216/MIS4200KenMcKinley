5a740eddcd0cc0de678734d8b7185de5f96a93b2
Mckinley <km956216@ohio.edu>
2020-01-27 11:35:41 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MIS4200KenMcKinley.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
